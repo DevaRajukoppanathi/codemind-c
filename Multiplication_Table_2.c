af3048b11977c0db462d35e3542531c8274dd318
Deva Raju <120381435+DevaRajukoppanathi@users.noreply.github.com>
2023-01-03 01:15:57 -0500

    #include<stdio.h> int main() {     int n,m,i;     scanf("%d%d",&n,&m);     for(i=1;i<=m;i++)     {         printf("%d x %d = %d ",n,i,n*i);     } }

--- Multiplication_Table_2.c
+++ Multiplication_Table_2.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int n,f,i;
-    scanf("%d%d",&n,&f);
-    for(i=1;i<=f;i++)
+    int n,m,i;
+    scanf("%d%d",&n,&m);
+    for(i=1;i<=m;i++)
     {
         printf("%d x %d = %d
 ",n,i,n*i);
     }
-    
 }
