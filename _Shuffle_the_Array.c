25b522211b4d015bb5c73cf00101be366816f35a
Deva Raju <120381435+DevaRajukoppanathi@users.noreply.github.com>
2023-06-02 19:38:03 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,j;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n/2;i++)     {         printf("%d %d ",a[i],a[n/2+i]);     } }

---
+++ _Shuffle_the_Array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,j;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n/2;i++)
+    {
+        printf("%d %d ",a[i],a[n/2+i]);
+    }
+}
+
