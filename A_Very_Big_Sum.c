9638a82fe23822f96d86f26ef7408847853f22bd
Deva Raju <120381435+DevaRajukoppanathi@users.noreply.github.com>
2023-06-02 19:27:38 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     long int a[n],s=0;     for(i=0;i<n;i++)     {         scanf("%ld",&a[i]);     }     for(i=0;i<n;i++)     {         s=s+a[i];     }     printf("%ld",s); }

---
+++ A_Very_Big_Sum.c
@@ -0,0 +1,30 @@
+
+
+
+ 
+
+
+
+
+
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    long int a[n],s=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%ld",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        s=s+a[i];
+    }
+    printf("%ld",s);
+}
+
+
+
+
+
