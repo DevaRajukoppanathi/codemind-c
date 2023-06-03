83e4b1a5b182a68a7338dd01a42a60e2769a9b54
Deva Raju <120381435+DevaRajukoppanathi@users.noreply.github.com>
2023-06-03 09:19:21 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<i;j++)         {             printf(" ");         }         for(k=1;k<=n;k++)         {             if(i==n || i==1 || k==1 || k==n )             printf("*");             else             printf(" ");         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,j;
+    int n,i,j,k;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=n;i>=1;i--)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<i;j++)
         {
-            printf("%d",j);
+            printf(" ");
+        }
+        for(k=1;k<=n;k++)
+        {
+            if(i==n || i==1 || k==1 || k==n )
+            printf("*");
+            else
+            printf(" ");
         }
         printf("
 ");
